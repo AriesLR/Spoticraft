a0c09eccd047838e8f30ea1267e20d7a71b0f574
Bobjobob24 <bobsgaming24@gmail.com>
2025-08-14 04:40:46 -0500

    TEST

---
+++ spoticraftTEST.lua
@@ -0,0 +1,24 @@
+-- spoticraft.lua
+local scriptUrl = "https://raw.githubusercontent.com/AriesLR/Spoticraft/refs/heads/main/spoticraft-logicTEST.lua"
+local targetDir = "/alr"
+
+-- Ensure the directory exists
+if not fs.exists(targetDir) then
+    print("Creating directory " .. targetDir)
+    fs.makeDir(targetDir)
+end
+
+-- Download the script if it doesn't exist
+local fileName = scriptUrl:match(".+/([^/]+)$")
+local fullPath = targetDir .. "/" .. fileName
+
+if not fs.exists(fullPath) then
+    print("Downloading " .. fileName .. " to " .. targetDir .. "...")
+    shell.run("wget " .. scriptUrl .. " " .. fullPath)
+else
+    print(fileName .. " already exists. Skipping download.")
+end
+
+-- Run the script
+print("Running " .. fullPath .. "...")
+shell.run(fullPath)
